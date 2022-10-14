f7e20297fa3b04a741f503901a57bf5585ada745
Guide.inc <guide.inc@Guideincs-MacBook-Pro.local>
2022-10-14 23:18:58 +0700

    init project

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php require('components/header.php'); ?>
+<?php require('components/nav.php'); ?>
+<?php require('components/home/banner.php'); ?>
+<?php require('components/footer.php'); ?>
+
+
+
